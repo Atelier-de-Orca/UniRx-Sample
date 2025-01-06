582e2c828c87bebc96c93ed9be2f3b0a47e7b157
Orca <rlatkdgus500@gmail.com>
2025-01-06 22:10:34 +0900

    :rocket: ObjectClick sample

---
+++ Assets/Samples/ObjectClick/Script/InfoPresenter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Samples.ObjectClick.Script
+{
+    public class InfoPresenter : MonoBehaviour
+    {
+        // [SerializeField]
+        // private CameraRayCaster cameraRayCaster;
+
+        // [SerializeField]
+        private Text infoText; 
+
+        private void Start() {
+            // cameraRayCaster.OnSelectObjectAsObservable
+            //     .Subscribe(x => {
+            //         infoText.text = x.ToString();
+            //         Debug.Log(x);
+            //     });
+        }
+    }
+}
