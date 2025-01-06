fe1c469b370ebcd008068956252b463b84d6514c
Orca <rlatkdgus500@gmail.com>
2025-01-06 22:06:04 +0900

    :rocket: Timer sample

---
+++ Assets/Samples/Timer/Script/TimerConfig.cs
@@ -0,0 +1,7 @@
+namespace Samples.Timer.Script
+{
+    public static class TimerConfig
+    {
+        public static float MAX_TIME = 300.0f;
+    }
+}
---
+++ Assets/Samples/Timer/Script/TimerPresenter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using R3;
+
+namespace Samples.Timer.Script
+{
+    public class TimerPresenter
+    {
+        private readonly TimerView _view;
+
+        public TimerPresenter(TimerView view)
+        {
+            this._view = view;
+        }
+        
+        public void StartTimer()
+        {
+            TimerModel.I.TimerReactiveProperty
+                .Subscribe(time =>
+                {
+                    this._view.UpdateView(time, TimerConfig.MAX_TIME);
+                })
+                .AddTo(this._view);
+        }
+        
+        public void ResetTimer()
+        {
+            TimerModel.I.TimerReactiveProperty.Value = TimerConfig.MAX_TIME;
+        }
+    }
+}
